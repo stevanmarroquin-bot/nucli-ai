65d35f4d9c66ce1c1b060a1cd8e201fb6d64bb06
Stevan Marroquin <stevan@nucli.solutions>
2026-06-08 12:06:45 -0600

    SEO + AIO: metadata, JSON-LD, sitemap, robots, llms.txt

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'GPTBot', allow: '/' },
+      { userAgent: 'ClaudeBot', allow: '/' },
+      { userAgent: 'Google-Extended', allow: '/' },
+      { userAgent: 'PerplexityBot', allow: '/' },
+      { userAgent: 'Applebot-Extended', allow: '/' },
+    ],
+    sitemap: 'https://nucli.solutions/sitemap.xml',
+  }
+}
